f77601f1f91b30ec7c435e1f7d0c1c5504d48457
joshuaIsCoding <joshua.albrigo97@gmail.com>
2025-12-23 11:30:43 -0500

    Add User entity and repository

---
+++ src/main/java/com/joshua/taskmanagement/repo/UserRepository.java
@@ -0,0 +1,12 @@
+package com.joshua.taskmanagement.repo;
+
+import com.joshua.taskmanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<Long, User> {
+    Optional<User> findByEmail(String email);
+}
